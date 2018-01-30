673250733901d0c6dcbed08abf29490ad0b7e23c
wcsadmin <wcsadmin@tcp_central.tcpcorp.local.com>
2018-01-30 12:05:18 -0500

    commented first test case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -7,12 +7,12 @@ import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
-
+/*
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
-    
+*/    
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
